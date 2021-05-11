48ded5944f3a130eb9a354d12e284344e9b6d8fb
Sadisha Eranga <sadishaeranga19678@gmail.com>
2021-05-11 16:09:17 +0530

    second commit

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,9 @@
 public class Runner {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(18);
+        list.add(45);
+        list.add(12);
+    }
 }
